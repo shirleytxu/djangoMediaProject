839b25a0527e68053acf7549d0995a0509336bf9
Shirley Xu <shirley.t.xu@gmail.com>
2021-04-15 00:39:09 -0700

    added comments, added link to create poll page

--- pollParty/models.py
+++ pollParty/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Poll(models.Model):
+    # Model for Poll
+
+    # Text Description for each Poll
     pollText = models.CharField(max_length=300)
+
+    # User that Created Poll
     userPosted = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Date and Time - Creation of Poll
     pubDate = models.DateTimeField('date published')
+
+    # Number of Up Votes for the Poll
     upVotes = models.IntegerField(default=0)
+
+    # Number of Down Votes for the Poll
     downVotes = models.IntegerField(default=0)
-
-
--- pollParty/views.py
+++ pollParty/views.py
@@ -1,17 +1,21 @@
 from django.utils import timezone
-
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Poll
 
-
-# Create your views here.
-
 def index(request):
+    """
+    index view- home page of pollParty
+    shows all existing polls from database
+    loads template pollParty/index.html
+    """
+
     if request.POST:
+        # Handles Post Operation
         if 'inputUsername' in request.POST.keys():
+            # Authenticate User
             user = authenticate(username=request.POST['inputUsername'],
                                 password=request.POST['inputPassword'])
             if user is not None:
@@ -25,8 +29,10 @@ def index(request):
     else:
         loggedIn = False
 
+    # Loads Template
     template = loader.get_template('pollParty/index.html')
 
+    # Loads Data from Database
     allPolls = Poll.objects.order_by('-pubDate')
 
     for poll in allPolls:
@@ -34,18 +40,26 @@ def index(request):
         poll.firstName = poster.first_name
         poll.lastName = poster.last_name
 
+    # Prepares Context for Template Rendering
     context = {'allPolls': allPolls, 'loggedIn': loggedIn,
                'user': request.user, }
     return HttpResponse(template.render(context, request))
 
 
 def createPoll(request):
+    """
+    createPoll view- page for user user to create a new poll
+    loads template pollParty/createPoll.html (simple line of text for now)
+    """
     if request.POST:
         newPoll = Poll(pollQuestion=request.POST['newPostText'],
                        userPosted=request.POST['userPosting'],
                        pubDate=timezone.now(), )
         newPoll.save()
 
+    # Loads Template
     template = loader.get_template('pollParty/createPoll.html')
-    context = {}
+
+    # Prepares Context
+    context = {}      # empty for now
     return HttpResponse(template.render(context, request))
