9f90de90912663c2f0e0fcc09c746845f5b71aa1
Shirley Xu <shirley.t.xu@gmail.com>
2021-05-31 23:24:53 -0700

    Add model relationship comments

--- pollParty/models.py
+++ pollParty/models.py
@@ -17,6 +17,7 @@ class Poll(models.Model):
     pollText = models.CharField(max_length=300)
 
     # User that Created Poll
+    # One user can create multiple Polls i.e. one-to-many relationship
     userPosted = models.ForeignKey(User, on_delete=models.CASCADE,
                                    related_name="userPostedPolls")
 
@@ -24,9 +25,13 @@ class Poll(models.Model):
     pubDate = models.DateTimeField('date published')
 
     # List of Users that Up Voted this Poll
+    # A Poll can have many users that upvote it and a user can upvote many polls
+    # i.e. many-to-many relationship
     upVotes = models.ManyToManyField(User, related_name="userUpvotedPolls")
 
     # List of Users that Down Voted this Poll
+    # A Poll can have many users that downvote it and a user can downvote many
+    # Polls i.e. many-to-many relationship
     downVotes = models.ManyToManyField(User, related_name="userDownvotedPolls")
 
     def __str__(self):
